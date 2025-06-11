009c10ef4ff67aa0d29c8833b0ac397a85dc3690
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-06-11 17:04:03 +0000

    my life is a joke like this api

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function gets a random joke from the API
+ * The 'async' keyword is used because it will take time from the internet to get data
+ */
+/* eslint-disable no-unused-vars */
+async function getJoke () {
+  try {
+    const response = await fetch(
+      'https://catfact.ninja/fact'
+    )
+    const data = await response.json()
+    console.log(data)
+
+    // Output the fun fact to the page
+    document.getElementById('show-joke').innerHTML =
+      '<b>Joke:</b> "' + data.fact + '"<br />'
+  } catch (error) {
+    console.error(error)
+  }
+}
