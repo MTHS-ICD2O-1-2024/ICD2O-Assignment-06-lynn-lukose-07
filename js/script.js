b5a1a16993d36a19a1a1373ca049abaec8c28703
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-06-16 21:14:33 +0000

    lintered!

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@
  * The 'async' keyword is used because it will take time from the internet to get data
  */
 /* eslint-disable no-unused-vars */
-async function getJoke() {
+async function getJoke () {
   try {
-    const resultJSON = await fetch("https://official-joke-api.appspot.com/random_joke")
+    const resultJSON = await fetch('https://official-joke-api.appspot.com/random_joke')
     const jsonData = await resultJSON.json()
     console.log(jsonData)
 
@@ -22,9 +22,9 @@ async function getJoke() {
     const punchline = jsonData.punchline
 
     // Output
-    document.getElementById("show-joke").innerHTML =
-      "<p>Joke: " + setup + "</br>Answer: " + punchline + " </p>"
+    document.getElementById('show-joke').innerHTML =
+      '<p>Joke: ' + setup + '</br>Answer: ' + punchline + ' </p>'
   } catch (error) {
-    document.getElementById("show-joke").innerHTML = "Sorry, an error has occurred. Please try again later."
+    document.getElementById('show-joke').innerHTML = 'Sorry, an error has occurred. Please try again later.'
   }
 }
